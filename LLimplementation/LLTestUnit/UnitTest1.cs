6694e7288cebd3ea0fd0f466d46a372e91ff0c37
teddydamian <teddy.damian@gmail.com>
2020-03-23 23:56:43 -0700

    LL Implementation

---
+++ LLimplementation/LLTestUnit/UnitTest1.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+using LLimplementation.Classes;
+
+namespace LLTestUnit
+{
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Instantiate Node and see if it has value.
+        /// </summary>
+        [Fact]
+        public void NodeClassHasValueProperty()
+        {
+            // Arrange Act
+            Node node = new Node();
+
+            // Assert
+            Assert.IsType<int>(node.Value);
+        }
+
+        /// <summary>
+        /// Instantiate Node and get the value.
+        /// </summary>
+        [Fact]
+        public void CanGetValueOfValuePropertyInNode()
+        {
+            Node node = new Node();
+            node.Value = 10;
+
+            Assert.Equal(10, node.Value);
+        }
+
+    }
+}
+

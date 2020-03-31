12b76f908e9724d5b1973b8a6f0d1272666be1c2
teddydamian <teddy.damian@gmail.com>
2020-03-30 18:27:46 -0700

    setup for test

---
+++ QueueWithStacks/QueueTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using QueueWithStacks.Classes;
+using Xunit;
+
+namespace QueueTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}

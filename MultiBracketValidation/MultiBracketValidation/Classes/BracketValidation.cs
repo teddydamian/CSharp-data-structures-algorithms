3c1a81197edb585f79170c8e1cbb51d16550857b
teddydamian <teddy.damian@gmail.com>
2020-04-01 22:20:08 -0700

    separate file

---
+++ MultiBracketValidation/MultiBracketValidation/Classes/BracketValidation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace MultiBracketValidation.Classes
+{
+    public class BracketValidation
+    {
+        /// <summary>
+        /// Method will put the closing bracket for every opening bracket.
+        /// If char match with top of stack, it will from stack until its empty.
+        /// If it doesnt match/dont pop from stack, it returns false.
+        /// Pair programming with Sue and Brody
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>True/False</returns>
+        public static bool MultiBracketValidate(string input)
+        {
+            Stack stax = new Stack();
+
+            foreach (char x in input)
+            {
+                if (x == '(')
+                {
+                    stax.Push(')');
+                }
+                else if (x == '[')
+                {
+                    stax.Push(']');
+                }
+                else if (x == '{')
+                {
+                    stax.Push('}');
+                }
+                else if (x == stax.Peek())
+                {
+                    stax.Pop();
+                }
+                else if (x == ')' || x == ']' || x == '}')
+                {
+                    return false;
+                }
+            }
+
+            if (stax.IsEmpty())
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

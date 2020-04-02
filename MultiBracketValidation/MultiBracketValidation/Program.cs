1e5e93863b6e7544481a6da2a5ce296797f09698
teddydamian <teddy.damian@gmail.com>
2020-04-02 12:18:18 -0700

    finished testing

--- MultiBracketValidation/MultiBracketValidation/Program.cs
+++ MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using MultiBracketValidation.Classes;
 
 namespace MultiBracketValidation
 {
@@ -8,54 +9,8 @@ namespace MultiBracketValidation
     {
         static void Main(string[] args)
         {
-
-        }
-        /// <summary>
-        /// Method will put the closing bracket for every opening bracket.
-        /// If char match with top of stack, it will from stack until its empty.
-        /// If it doesnt match/dont pop from stack, it returns false.
-        /// Pair programming with Sue and Brody
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns>True/False</returns>
-        public bool Validate(string input)
-        {
-            Stack stax = new Stack();
-
-            foreach (char x in input)
-            {
-                if (x == '(')
-                {
-                    stax.Push(')');
-                }
-                else if (x == '[')
-                {
-                    stax.Push(']');
-                }
-                else if (x == '{')
-                {
-                    stax.Push('}');
-                }
-                else if (x == stax.Peek())
-                {
-                    stax.Pop();
-                }
-                else if (x == ')' || x == ']' || x == '}')
-                {
-                    return false;
-                }
-            }
-
-            if (stax.IsEmpty())
-            {
-                return true;
-            }
-
-            else
-            {
-                return false;
-            }
+            
+            
         }
     }
-
 }

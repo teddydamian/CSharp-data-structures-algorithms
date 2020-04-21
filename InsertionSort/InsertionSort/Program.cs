25c1c88b83d15c3fbac480926f631e8de7d02543
teddydamian <teddy.damian@gmail.com>
2020-04-20 18:29:12 -0700

    project

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] InsertionSort(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                int j = 0;
+            }
+        }
+    }
+}

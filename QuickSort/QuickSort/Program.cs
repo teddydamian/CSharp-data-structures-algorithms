d0cc722d45279cf3a74e602b4a892e25165d97d5
teddydamian <teddy.damian@gmail.com>
2020-04-22 20:31:12 -0700

    algo done

--- QuickSort/QuickSort/Program.cs
+++ QuickSort/QuickSort/Program.cs
@@ -2,11 +2,49 @@
 
 namespace QuickSort
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[] { 8, 4, 23, 42, 16, 15 };
+            QuickSortArray(array, 0, array.Length-1);
+        }
+
+        public static int[] QuickSortArray(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int position = Partition(arr, left, right);
+
+                QuickSortArray(arr, left, position - 1);
+                QuickSortArray(arr, position + 1, right);
+            }
+            return arr;
+        }
+
+        static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+
+            int low = left - 1;
+            for (int i = left; i < right; i++)
+            {
+                if (arr[i] <= pivot)
+                {
+                    low++;
+                    Swap(arr, i, low);
+                }
+            }
+            Swap(arr, right, low + 1);
+            return low + 1;
+        }
+
+        static void Swap(int[] arr, int i, int low)
+        {
+            int temp;
+            temp = arr[i];
+            arr[i] = arr[low];
+            arr[low] = temp;
         }
     }
 }

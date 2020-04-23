afd7d2d4a6a8be857be7a60fa2b9094ece922352
teddydamian <teddy.damian@gmail.com>
2020-04-22 20:44:55 -0700

    Done with test and summary comments

--- QuickSort/QuickSort/Program.cs
+++ QuickSort/QuickSort/Program.cs
@@ -6,10 +6,15 @@ namespace QuickSort
     {
         public static void Main(string[] args)
         {
-            int[] array = new int[] { 8, 4, 23, 42, 16, 15 };
-            QuickSortArray(array, 0, array.Length-1);
         }
 
+        /// <summary>
+        /// This method will Sort the array by calling Partition method.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         public static int[] QuickSortArray(int[] arr, int left, int right)
         {
             if (left < right)
@@ -22,6 +27,11 @@ namespace QuickSort
             return arr;
         }
 
+        /// <summary>
+        /// This method will compare each value to pivot value. If it is less or equal to pivot value, it will call the method swap.
+        /// Pivot is usually determined as the last value in index.
+        /// It will place the pivot index in the middle of the array where left of the pivot values are less and right is greater than the pivot value.
+        /// </summary>
         static int Partition(int[] arr, int left, int right)
         {
             int pivot = arr[right];
@@ -39,6 +49,9 @@ namespace QuickSort
             return low + 1;
         }
 
+        /// <summary>
+        /// This method will swap value in an array
+        /// </summary>
         static void Swap(int[] arr, int i, int low)
         {
             int temp;
---
+++ QuickSort/QuickSortTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+using QuickSort;
+
+namespace QuickSortTest
+{
+    public class UnitTest1
+    {
+        /// <summary>
+        /// This test takes in 4 different arrays, and sort them.
+        /// </summary>
+        [Theory]
+        [InlineData(new int[] { 8, 4, 23, 42, 16, 15 }, new int[] { 4, 8, 15, 16, 23, 42 })]
+        [InlineData(new int[] { 20, 18, 12, 8, 5, -2 }, new int[] { -2, 5, 8, 12, 18, 20 })]
+        [InlineData(new int[] { 5, 12, 7, 5, 5, 7 }, new int[] { 5, 5, 5, 7, 7, 12 })]
+        [InlineData(new int[] { 2, 3, 5, 7, 13, 11 }, new int[] { 2, 3, 5, 7, 11, 13 })]
+        public void TestForAllArrayTypes(int[] input, int[] expected)
+        {
+            int[] result = QuickSort.Program.QuickSortArray(input, 0, input.Length - 1);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}

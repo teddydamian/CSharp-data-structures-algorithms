9994fd9949e0277b3f36f5e9b57535a2d480097b
teddydamian <teddy.damian@gmail.com>
2020-05-11 13:12:09 -0700

    graphs test

---
+++ Graphs/Graphs/Classes/Edge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs.Classes
+{
+ 
+    public class Edge<T>
+    {
+        public int Weight { get; set; }
+        public Vertex<T> Vertex { get; set; }
+    }
+    
+}
---
+++ Graphs/Graphs/Classes/Vertex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Graphs.Classes
+{
+    public class Vertex<T>
+    {
+        public T Value { get; set; }
+
+        public Vertex(T value)
+        {
+            Value = value;
+        }
+    }
+}

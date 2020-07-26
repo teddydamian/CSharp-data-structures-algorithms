e7873595c3b1a5edcf0d7163bd5411e67d9d4004
Teddy Damian <Teddy Damian@gmail.com>
2020-07-25 19:39:07 -0700

    FakeBinary

---
+++ FakeBinary/FakeBinary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Dynamic;
+using System.Text;
+
+namespace FakeBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.");
+            Console.WriteLine("Test numbers are: 18376");
+            string test = FakeBin("18376");
+            Console.WriteLine("Press any button to continue");
+            Console.ReadLine();
+
+            Console.WriteLine($"Result: {test}");
+        }
+
+        public static string FakeBin(string x)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char a in x)
+            {
+                result.Append(a >= '5' ? '1' : '0');
+            }
+            return result.ToString();
+        }
+    }
+}
